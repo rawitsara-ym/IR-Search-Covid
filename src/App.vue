7ef05801c706e750a75e2374d88408334fc64a56
Yingmook <rawitsara.nu@gmail.com>
2021-12-14 00:19:37 +0700

    show document component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='title'>
+    <img class='icon flip' alt="covid-19 logo" src="./assets/covid_icon.png">
+    <h1>COVID-19 Research</h1>
+    <img class='icon' alt="covid-19 logo" src="./assets/covid_icon.png">
+  </div>
+  <ShowDocument v-bind:doc="doc"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShowDocument from './components/ShowDocument.vue'
 
 export default {
+  data() {
+    return { doc: {
+                    title: "COVID-19 virtual patient cohort reveals immune mechanisms driving disease outcomes", 
+                    published: '01-01-2020', 
+                    authors: ["Rutu Karia", "Ishita Gupta", "Harshwardhan Khandait", "Ashima Yadav", "Anmol Yadav"],
+                    abtract: 'To understand the diversity of immune responses to SARS-CoV-2 and distinguish features that 39 predispose individuals to severe COVID-19  we developed a mechanistic  within-host mathematical 40 model and virtual patient cohort. Our results indicate that virtual patients with low production rates of 41 infected cell derived IFN subsequently experienced highly',
+                    url: 'https://journals.plos.org/plospathogens/article?id=10.1371/journal.ppat.1009753'
+                    }
+            }
+  },
   name: 'App',
   components: {
-    HelloWorld
+    ShowDocument
   }
 }
 </script>
@@ -20,7 +34,20 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* color: #2c3e50; */
+  margin: 20px 60px 0px 60px;
+}
+.title {
+  font-size: large;
+  display: flex;
+  justify-content: center;
+}
+.icon {
+  width: 60px;
+  height: 60px;
+  padding: 20px;
+}
+.flip {
+  transform: scaleX(-1);
 }
 </style>
---
+++ src/components/ShowDocument.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="grid-container">
+    <p style="font-weight: bold;">Title</p>
+    <p>{{ doc.title }}</p>
+  </div>
+  <hr>
+  <div class="grid-container">
+    <p style="font-weight: bold;">Published</p>
+    <p>{{ doc.published }}</p>
+  </div>
+  <hr>
+  <div class="grid-container">
+    <p style="font-weight: bold;">Authors</p>
+    <p>{{ doc.authors.reduce((prev, curr) => `${prev}, ${curr}`) }}</p>
+  </div>
+  <hr>
+  <div class="grid-container">
+    <p style="font-weight: bold;">Abtract</p>
+    <p>{{ doc.abtract }}</p>
+  </div>
+  <hr>
+  <div class="grid-container">
+    <p style="font-weight: bold;">URL</p>
+    <p><a :href="doc.url" target="_blank">{{ doc.url }}</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowDocument',
+  props: {
+    doc: Object,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: 150px auto;
+  justify-content: flex-start;
+  text-align: left;
+}
+
+</style>
